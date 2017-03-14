58a75c235745901eeffa3d51a4748a32d14cc005
Garri Djavadyan <g.djavadyan@gmail.com>
2017-03-14 16:26:21 +0500

    Add support for date format specification. It is used to construct backup filename. See README. Change for CLI arguments. The arguments no longer depends on order. The BACKUP_TYPE option renamed to BACKUP_DST to more precisely reflect it's purpose. Code refactoring. Documentation update.

--- sysbackup.sh
+++ sysbackup.sh
@@ -19,7 +19,7 @@
 TARGETS="/ /home /usr /var"		# Filesystems to backup
 EXCLUDE="--exclude=/mnt"		# Excluded directories from backup
 BACKUP_DIR="/backup/server1"		# Full path to directory contining system and mysql subdirs
-BACKUP_TYPE="local"			# Remote or local backup destination (ssh, local)
+BACKUP_DST="local"			# Backup destination (local, remote)
 COMPRESS_TYPE="gzip"			# Valid values are gzip, bzip2, xz and gpg (for encryption)
 
 # E-mail reports configuration
@@ -45,13 +45,118 @@ DB_NAME[2]="second"			# Second, third, ... database, if required
 # The code below is not intended for direct modification.
 # Change it only if you are sure it is needed.
 
-VERSION="0.69"
+print_usage() {
+  echo "Usage: $0 [--type=system|mysql] [--notify=yes|no] [--dateformat=weekday|monthday|month]"
+  exit 1
+}
+
+# Check local or remote file size in MB
+check_size() {
+    if [ ${BACKUP_DST} = "remote" ]; then
+	ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "du -m ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}" | cut -f 1
+    else
+	du -m ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT} | cut -f 1
+    fi
+}
+
+send_email() {
+    [ -x ${MAIL_CMD} ] || { echo "Mail utility not installed. Skip email sending."; return 1; }
+    echo -e "Date: $(date)\nBackup Type: ${6}\nHost: ${1}\nFile: ${2}\nSize (MB): ${3}\nDuration (sec): ${4}\nDestination Host: ${5}\n\n---\nArchive created using SysBackup-${VERSION}" | ${MAIL_CMD} -s "SysBackup completed on ${1}" ${CONTACT}
+    echo "E-mail sent to ${CONTACT}."
+}
+
+main() {
+  if [ -f ${LOCK_FILE} ]; then
+    echo -e "SysBackup already started. Abort.\nIf you are sure it is not true, please remove file ${LOCK_FILE} manually."
+    exit 1
+  else
+    starttime=$(date +%s)
+    touch ${LOCK_FILE}
+    trap "rm -f ${LOCK_FILE}; echo -e '\nOperation Abored!'; exit 1" SIGINT SIGTERM
+    ${BACKUP_TYPE} || { rm -f ${LOCK_FILE}; echo -e "SysBackup failed."; exit 1; }
+    [[ "${BACKUP_DST}" == "remote" ]] && destination="${REMOTE_HOST}" || destination="localhost"
+    duration=$(( $(date +%s) - $starttime ))
+    if [[ "${NOTIFY}" == "yes" ]]; then
+      if [[ "${BACKUP_TYPE}" == "system" ]]; then
+        send_email $(hostname) ${BACKUP_FILE}.${EXT} $(check_size) ${duration} ${destination} ${BACKUP_TYPE}
+      else
+        send_email $(hostname) unknown unknown ${duration} ${destination} ${BACKUP_TYPE}
+      fi
+    fi
+    rm -f ${LOCK_FILE}
+    echo "SysBackup completed without errors."
+    exit 0
+  fi
+}
+
+system() {
+  [ -n "$(which tar)" ] || { echo "Tar archiver is not installed."; return 1; } 
+  if [ ${BACKUP_DST} = "remote" ]; then
+    tar -cvf - --one-file-system ${EXCLUDE} ${TARGETS} | ${COMPRESSOR} | \
+      ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cat > ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}"
+  else
+    tar -cvf - --one-file-system ${EXCLUDE} ${TARGETS} | ${COMPRESSOR} > ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}
+  fi
+}
+
+mysql() {
+  [ -n "$(which mysqldump)" ] || { echo "Mysqldump is not installed."; return 1; } 
+  if [ ${BACKUP_DST} = "remote" ]; then
+    for i in ${DB_NAME[@]}; do
+      mysqldump ${DB_OPTS} -u ${DB_USER} -p${DB_PASS} ${i} | ${COMPRESSOR} | \
+        ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cat > ${BACKUP_DIR}/mysql/${i}-${DATEFORMAT}.sql.${EXT}"
+    done
+  else
+    for i in ${DB_NAME[@]}; do
+      mysqldump ${DB_OPTS} -u ${DB_USER} -p${DB_PASS} ${i} | ${COMPRESSOR} > ${BACKUP_DIR}/mysql/${i}-${DATEFORMAT}.sql.${EXT}
+    done
+  fi
+}
+
+
+# Parse CLI options
+for CMD_ARG; do
+  case ${CMD_ARG} in
+    --type=system)
+      BACKUP_TYPE="system"
+      ;;
+    --type=mysql)
+      BACKUP_TYPE="mysql"
+      ;;
+    --type=*)
+      echo "Specified backup type is not supported. Using default 'system'."
+      ;;
+    --notify=yes)
+      NOTIFY="yes"
+      ;;
+    --dateformat=weekday)
+      DATEFORMAT=$(date +%A)
+      ;;
+    --dateformat=monthday)
+      DATEFORMAT=$(date +%d)
+      ;;
+    --dateformat=month)
+      DATEFORMAT=$(date +%B)
+      ;;
+    --dateformat=*)
+      echo "Specified date format is not supported. Using default 'monthday'."
+      ;;
+    *)
+      print_usage
+      ;;
+  esac
+done
+
+
+# Set defaults
+VERSION="0.70"
 HOME="/root/"
 PATH="${PATH}:/usr/local/bin:/usr/local/sbin"
 LOCK_FILE=/tmp/sysbackup.lock
-DATE_DOM=`date "+%d"`		# Day of Month
-DATE_DOW=`date "+%A"`		# Day of Week
-BACKUP_FILE="sysbackup-${DATE_DOM}.tar"
+BACKUP_TYPE=${BACKUP_TYPE:-"system"}
+NOTIFY=${NOTIFY:-"no"}
+DATEFORMAT=${DATEFORMAT:-$(date +%d)}
+BACKUP_FILE="sysbackup-${DATEFORMAT}.tar"
 
 # Adapt command and file extension for compression type
 case ${COMPRESS_TYPE} in
@@ -76,79 +181,5 @@ case ${COMPRESS_TYPE} in
     exit 1
 esac
 
-# Check local or remote file size in MB
-check_size() {
-    if [ ${BACKUP_TYPE} = "ssh" ]; then
-	ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "du -m ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}" | cut -f 1
-    else
-	du -m ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT} | cut -f 1
-    fi
-}
-
-# Usage: send_email host file size duration destination backup_type
-send_email() {
-    [ -x ${MAIL_CMD} ] || { echo "Mail utility not installed. Skip email sending."; return 1; }
-    echo -e "Date: $(date)\nBackup Type: ${6}\nHost: ${1}\nFile: ${2}\nSize (MB): ${3}\nDuration (sec): ${4}\nDestination Host: ${5}\n\n---\nArchive created using SysBackup-${VERSION}" | ${MAIL_CMD} -s "SysBackup completed on ${1}" ${CONTACT}
-    echo "E-mail sent to ${CONTACT}."
-}
-
-common() {
-  backup_type=${1}
-  if [ -f ${LOCK_FILE} ]; then
-    echo -e "SysBackup already started. Abort.\nIf you are sure it is not true, please remove file ${LOCK_FILE} manually."
-    exit 1
-  else
-    starttime=$(date +%s)
-    touch ${LOCK_FILE}
-    trap "rm -f ${LOCK_FILE}; echo -e '\nOperation Abored!'; exit 1" SIGINT SIGTERM
-    ${backup_type} || { rm -f ${LOCK_FILE}; echo -e "SysBackup failed."; exit 1; }
-    [ ${BACKUP_TYPE} = "ssh" ] && destination="${REMOTE_HOST}" || destination="localhost"
-    duration=$(( $(date +%s) - $starttime ))
-    [ "${2}" = "notify" ] && {
-      if [ ${backup_type} = "system" ]; then
-        send_email $(hostname) ${BACKUP_FILE}.${EXT} $(check_size) ${duration} ${destination} ${backup_type}
-      else
-        send_email $(hostname) unknown unknown ${duration} ${destination} ${backup_type}
-      fi
-    }
-    rm -f ${LOCK_FILE}
-    echo "SysBackup completed without errors."
-    exit 0
-  fi
-}
-
-system() {
-  [ -n "$(which tar)" ] || { echo "Tar archiver is not installed."; return 1; } 
-  if [ ${BACKUP_TYPE} = "ssh" ]; then
-    tar -cvf - --one-file-system ${EXCLUDE} ${TARGETS} | ${COMPRESSOR} | \
-      ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cat > ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}"
-  else
-    tar -cvf - --one-file-system ${EXCLUDE} ${TARGETS} | ${COMPRESSOR} > ${BACKUP_DIR}/system/${BACKUP_FILE}.${EXT}
-  fi
-}
-
-mysql() {
-  [ -n "$(which mysqldump)" ] || { echo "Mysqldump is not installed."; return 1; } 
-  if [ ${BACKUP_TYPE} = "ssh" ]; then
-    for i in ${DB_NAME[@]}; do
-      mysqldump ${DB_OPTS} -u ${DB_USER} -p${DB_PASS} ${i} | ${COMPRESSOR} | \
-        ssh -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cat > ${BACKUP_DIR}/mysql/${i}-${DATE_DOW}.sql.${EXT}"
-    done
-  else
-    for i in ${DB_NAME[@]}; do
-      mysqldump ${DB_OPTS} -u ${DB_USER} -p${DB_PASS} ${i} | ${COMPRESSOR} > ${BACKUP_DIR}/mysql/${i}-${DATE_DOW}.sql.${EXT}
-    done
-  fi
-}
-
-case ${1} in
-    system)
-	common ${1} ${2}
-	;;
-    mysql)
-	common ${1} ${2}
-	;;
-    *)
-	echo "Usage: ${0} {system|mysql} [notify]"
-	;;
-esac
+# Call main function
+main
